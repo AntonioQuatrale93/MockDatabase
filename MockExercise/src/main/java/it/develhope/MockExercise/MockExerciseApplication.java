27751848a175bda7b68483632d254cdaac1e4bfa
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-07-19 15:27:43 +0200

    Add files via upload

---
+++ MockExercise/src/main/java/it/develhope/MockExercise/MockExerciseApplication.java
@@ -0,0 +1,13 @@
+package it.develhope.MockExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockExerciseApplication.class, args);
+	}
+
+}
---
+++ MockExercise/src/test/java/it/develhope/MockExercise/MockExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.MockExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
